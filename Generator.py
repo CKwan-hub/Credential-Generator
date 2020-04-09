89f3c274bc7f47ac7d22c794a1974d02fea13e27
CKwan-Hub <ckwandrans@gmail.com>
2020-04-08 20:27:41 -0400

    Added a million result list

--- Generator.py
+++ Generator.py
@@ -27,7 +27,7 @@ extra_length = [0, 1, 2, 3, 4]
 # list of text to act as the email base value.
 # can change to short_text, medium_text, long_text, or longest_text
 # will create 300, 1750, 7,500 or 100,000 results
-email_text = json.loads(open('longest_text.json').read())
+email_text = json.loads(open('medium_text.json').read())
 
 # open output.txt in append mode.
 output_file = open('output.txt', 'a')
