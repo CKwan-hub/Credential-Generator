6b1d8c6507f7b2be389d7898ea7f36e7f07fceb7
CKwan-Hub <ckwandrans@gmail.com>
2020-05-08 02:08:41 -0400

    added realistic passwords blended in at random

--- Generator.py
+++ Generator.py
@@ -5,6 +5,9 @@ import random
 import string
 import json
 
+# list of text to act as the email base value.
+email_text = json.loads(open('medium_text.json').read())
+
 # string of ascii letters in both upppercase & lowercase + string of digits  + spec characters
 chars = string.ascii_letters + string.digits + '!?@#$%&*'
 
@@ -21,31 +24,44 @@ password_length = [6, 7, 8, 9, 10, 11, 12]
 # choices for generated extra email numbers length.
 extra_length = [0, 1, 2, 3, 4]
 
+
 # url to which the data can be sent.
 # url = '#'
-
-# list of text to act as the email base value.
-email_text = json.loads(open('medium_text.json').read())
 
 # open output.txt in append mode.
 output_file = open('output.txt', 'a')
 
 for email_data in email_text:
+    # choices for generated password additions
+    pass_length = [1, 2, 3]
 
-    # additional values for randomly adding a second word to email.
-    name_random = ["", random.choice(email_text), ""]
+    pass_digits = ''.join(random.choice(chars)
+                          for i in range(random.choice(pass_length)))
 
     # take a random amount of digits, at a random length between 0 and 4.
     name_digits = ''.join(random.choice(string.digits)
                           for i in range(random.choice(extra_length)))
+
+    # additional values for randomly adding a second word to email.
+    name_random = ["", random.choice(email_text), ""]
+
+    # Strong password choice.
+    strongPass = ''.join(random.choice(chars)
+                         for i in range(random.choice(password_length)))
+
+    # Realistic password generation.
+    realPass = ''.join(random.choice(email_text) +
+                       random.choice(name_random) + pass_digits)
+
+    # Realistic password chance.
+    passSelect = [strongPass, realPass, realPass]
 
     # lowercase values from email_text + random digits + random choice of email suffix.
     username = email_data.lower() + random.choice(name_random) + \
         name_digits + random.choice(email_list)
 
     # random selection of upper & lower case characters/digits/special characters at a length of 6-12.
-    password = ''.join(random.choice(chars)
-                       for i in range(random.choice(password_length)))
+    password = random.choice(passSelect)
 
     # Post to the specified url, don't redirect and pass usernames and passwords.
     # !!Need to specify the var for username and password from submission form.
